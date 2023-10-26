5a590fe0de090dc1026d42ea58c0228b4e46d6ad
BerezaOlena <bereza.olena@gmail.com>
2023-10-26 20:57:43 +0300

    window size firefox

--- conftest.py
+++ conftest.py
@@ -30,25 +30,35 @@ def browser(request):
 
     if browser_name == "chrome":
         print(f"\nstart {browser_name} browser for test..")
-        # opts_chrome.add_argument('window_size=1920, 1080')
+        opts_chrome.add_argument('window_size=1200, 800')
         browser = webdriver.Chrome(options=opts_chrome)
     elif browser_name == "firefox":
         print(f"\nstart {browser_name} browser for test..")
-        # opts_firefox.add_argument('window_size=1920, 1080')
+        opts_firefox.add_argument('--width=1200')
+        opts_firefox.add_argument('--height=800')
         browser = webdriver.Firefox(options=opts_firefox)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
-
-    if browser_window_size == "max":
-        browser.maximize_window()
-    elif browser_window_size == "norma":
-        browser.set_window_size(1200, 800)
-    else:
-        print("must be max or norma")
+    #
+    # if browser_window_size == "max":
+    #     browser.maximize_window()
+    # elif browser_window_size == "norma":
+    #     browser.set_window_size(1200, 800)
+    # else:
+    #     print("must be max or norma")
 
     yield browser
     print("\nquit browser..")
     browser.quit()
+
+# pytest -s -v
+# pytest -s -v --browser_name="firefox"
+# pytest -s -v --browser_mode="gui"
+# pytest -s -v --browser_window_size="max"
+# pytest -s -v --browser_name="firefox" --browser_mode="gui"
+# pytest -s -v --browser_name="firefox" --browser_window_size="max"
+# pytest -s -v --browser_mode="gui" --browser_window_size="max"
+# pytest -s -v --browser_name="firefox" --browser_mode="gui" --browser_window_size="max"
 
 
 
